2bf3466022fa988d468188ffd2e45eb78e4e4c70
Toan Tran <ttkbnt@hotmail.com>
2018-08-12 16:54:45 -0500

    Collected a single payment's breakdown

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const credentials = require('./credentials');
+const fs = require('fs');
 
 (async() => {
   // const browser = await puppeteer.launch();
@@ -7,47 +9,67 @@ const puppeteer = require('puppeteer');
 
   await page.goto('https://www.nelnet.com/welcome');
 
-  // fill in #username and click #submit-username
+  // Fill in #username and click #submit-username
   await page.waitForSelector('#username', {timeout: 10000});
-  let usernameField = await page.$('#username');
-  await page.waitForFunction(function(input) {
-    return !input.disabled;
-  }, {polling: 2000, timeout: 10000}, usernameField);
+  // Nelnet disables input until page is finished loading
+  await page.waitForFunction(
+    (input) => !input.disabled,
+    {polling: 2000, timeout: 10000},
+    await page.$('#username'));
   await page.focus('#username');
-  await page.keyboard.type('');
+  await page.keyboard.type(credentials.USERNAME);
   await page.click('#submit-username');
 
   // fill in #Password and click #submit-password
   await page.waitForSelector('#Password', {timeout: 10000});
-  let passwordField = await page.$('#Password');
-  await page.waitForFunction(function(input) {
-    return !input.disabled;
-  }, {polling: 2000, timeout: 10000}, passwordField);
+  // Nelnet disables input until username is submitted
+  await page.waitForFunction(
+    (input) => !input.disabled,
+    {polling: 2000, timeout: 10000},
+    await page.$('#Password'));
   await page.focus('#Password');
-  await page.keyboard.type('');
+  await page.keyboard.type(credentials.PASSWORD);
   await page.click('#submit-password');
 
-  // go to history
+  // Go to history
   await page.waitForNavigation();
   await page.goto('https://www.nelnet.com/Payment/History');
 
-  // expand full history
+  // Expand full history
   await page.waitForSelector('ul.pager li:nth-child(2)');
   await page.click('ul.pager li:nth-child(2)');
+  // Wait for table to expand to something greater than 10
+  await page.waitForSelector('tbody tr:nth-child(11)');
 
   // click each item and collect data
-  let paymentCount = await page.$('tr').length;
-  let paymentRecord = await page.$('tr:nth-child(50) td:nth-child(1) a');
+  let paymentCount = await page.$('tbody tr').length;
+  // foreach row
+  let paymentRecord = await page.$('tbody tr:nth-child(50) td:nth-child(1) a');
+  var paymentDate = await page.$eval('tbody tr:nth-child(50) td:nth-child(1) a', element => element.innerText.trim());
 
-  // paymentRecord is null
   await paymentRecord.click();
+  await page.waitForSelector('#payment-info')
 
-  // let paymentDate = await page.$eval('#payment-info form .row .row:nth-child(1) .col-xs-5', element => element.innerText);
-  // console.log(paymentDate);
-  // let paymentDetailsTable = page.$('table');
+  // Get all the rows (payment groups)
+  let paymentGroups = await page.$$eval('tbody tr', async(rows) => {
+    rows.pop(); // Last table row is totals
 
+    return rows.map(tr => {
+      let cells = Array.from(tr.querySelectorAll('td')).map(td => td.innerText.trim());
 
-  await page.click('a.back-icon');
+      return {
+        paymentDate: paymentDate,
+        groupName: cells[0],
+        appliedToPrincipal: cells[1],
+        appliedToInterest: cells[2],
+        appliedToFees: cells[3]
+      };
+    })
+  });
+
+  console.log(paymentGroups);
+
+  // await page.click('a.back-icon');
 
   // await browser.close();
 })();
