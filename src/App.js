d8d943caf69abc5c7dac34db72e03cab0b6c0ae4
Toan Tran <ttkbnt@hotmail.com>
2018-09-22 15:48:58 -0500

    Graphed amounts applied to principal
    - Added react-vis

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import GraphSvg from './GraphSvg';
+import { groups, payments } from './data.json';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Nelnet-vis</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <GraphSvg />
+        <GraphSvg groups={groups} payments={payments} />
       </div>
     );
   }
--- src/GraphSvg.js
+++ src/GraphSvg.js
@@ -1,25 +1,99 @@
 import React from 'react';
+import 'react-vis/dist/style.css';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries } from 'react-vis';
 
 export default class GraphSvg extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this._getState(props.data);
+    this.state = this._getState(props.groups, props.payments);
   }
 
   render() {
+    var allSeries = this._generateSeries();
+
     return (
-      <svg id="graphSvg" width={this.state.width} height={this.state.height}>
-        <circle cx="50" cy="50" r="10" style={{fill: "red"}} />
-      </svg>
+      <div style={{margin: 'auto', width: this.state.width}}>
+        <XYPlot width={this.state.width} height={this.state.height} xType="time" style={{padding: '10px'}}>
+          <XAxis />
+          <YAxis />
+          <HorizontalGridLines />
+          <VerticalGridLines />
+          {allSeries}
+        </XYPlot>
+      </div>
     );
   }
 
-  _getState(data) {
-    var width = document.body.clientWidth;
+  _generateSeries() {
+    var allSeries = [];
+
+    for(let group of this.state.groups) {
+      var groupPayments = this.state.paymentsByGroup[group];
+      var series = [{
+        x: groupPayments[0].paymentDate,
+        y: groupPayments[0].appliedToPrincipal
+      }];
+
+      for(var i = 1; i < groupPayments.length; i++) {
+        series.push({
+          x: groupPayments[i].paymentDate,
+          y: series[series.length - 1].y
+        });
+
+        series.push({
+          x: groupPayments[i].paymentDate,
+          y: series[series.length - 1].y
+             + groupPayments[i].appliedToPrincipal
+        });
+      }
+
+      // series.push({
+      //   x: new Date(this.state.maxDate),
+      //   y: series[series.length - 1].y
+      // });
+
+      allSeries.push(
+        <LineSeries key={group} data={series} />
+      );
+    }
+
+    return allSeries;
+  }
+
+  _getState(groups, payments) {
+    var groupNames = Object.keys(groups);
+    var paymentsByGroup = {};
+    for(let group of groupNames) {
+      paymentsByGroup[group] = [];
+    }
+
+    var minDate = new Date(payments[0].paymentDate);
+    var maxDate = minDate;
+
+    var paymentsByGroup = payments.reduce((aggregate, current) => {
+      minDate = Math.min(minDate, current.paymentDate);
+      maxDate = Math.max(minDate, current.paymentDate);
+
+      aggregate[current.groupName].push({
+        paymentDate: new Date(current.paymentDate),
+        appliedToPrincipal: current.appliedToPrincipal,
+        appliedToInterest: current.appliedToInterest
+      });
+
+      return aggregate;
+    }, paymentsByGroup);
+
+    for(var group in paymentsByGroup) {
+      paymentsByGroup[group].sort((a,b) => a.paymentDate - b.paymentDate);
+    }
 
     return {
+      groups: groupNames,
       height: 600,
-      width: width
+      maxDate: maxDate,
+      minDate: minDate,
+      paymentsByGroup: paymentsByGroup,
+      width: 800
     };
   }
 }
