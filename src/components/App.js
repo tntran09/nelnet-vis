e4da2f7632ebd48bcb1be2930f863ecc35e1262f
Toan Tran <ttkbnt@hotmail.com>
2018-11-03 17:33:41 -0500

    Install material-ui, arrange stuff into Papers

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Paper from '@material-ui/core/Paper';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import GraphSvg from './GraphSvg';
 import { groups, payments } from '../data.json';
 
@@ -7,10 +13,28 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Nelnet-vis</h1>
-        </header>
-        <GraphSvg groups={groups} payments={payments} />
+        <Grid container spacing={16}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">Nelnet Vis</Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Grid item xs={3}>
+            <Paper>
+              <MenuList>
+                <MenuItem selected={true}>Payments Applied</MenuItem>
+                <MenuItem>Principal Remaining</MenuItem>
+                <MenuItem>Applied to Interest</MenuItem>
+              </MenuList>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper>
+              <GraphSvg groups={groups} payments={payments} />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
